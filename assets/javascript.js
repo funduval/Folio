256c5a5d9e9d0c8bddc3801e925307bb556fe53d
funduval <fundaduval@gmail.com>
2017-06-23 16:48:27 -0400

    Adding MediaQueries

--- assets/javascript.js
+++ assets/javascript.js
@@ -3,7 +3,7 @@
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 380,
+      "value": 200,
       "density": {
         "enable": true,
         "value_area": 800
@@ -51,12 +51,12 @@ particlesJS("particles-js", {
       "enable": true,
       "distance": 150,
       "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
+      "opacity": 1,
+      "width": 2
     },
     "move": {
       "enable": true,
-      "speed": 6,
+      "speed": 1,
       "direction": "none",
       "random": false,
       "straight": false,
