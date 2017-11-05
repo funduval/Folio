debfe470bb96aec88acfefb254e9e94fa9bbf5fd
funduval <fundaduval@gmail.com>
2017-11-05 12:53:37 -0500

    color scheme

--- js/resume.js
+++ js/resume.js
@@ -1,15 +1,22 @@
 (function($) {
-  "use strict"; // Start of use strict
+  'use strict'; // Start of use strict
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+      location.hostname == this.hostname
+    ) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top)
-        }, 1000, "easeInOutExpo");
+        $('html, body').animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1000,
+          'easeInOutExpo'
+        );
         return false;
       }
     }
@@ -24,5 +31,4 @@
   $('body').scrollspy({
     target: '#sideNav'
   });
-
 })(jQuery); // End of use strict
